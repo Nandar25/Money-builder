acb7a2b7aa7d43a97017c7f0624d0348f4c2b370
Nandar25 <lin_nandar@yahoo.com>
2026-04-21 21:05:15 +0100

    add main.jsx

--- money builder/src/main.jsx
+++ money builder/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+//import React DOM to render the app in the browser
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+//This connect React to the HTNL file (index.html)
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
